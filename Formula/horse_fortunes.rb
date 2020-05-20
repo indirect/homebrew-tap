a2f076ef0178ff24f5f240ad08b7e4b50c4c1335
Andre Arko <andre@arko.net>
2020-05-19 17:58:14 -0700

    put the fortune file into the repo

--- Formula/horse_fortunes.rb
+++ Formula/horse_fortunes.rb
@@ -2,7 +2,7 @@ class HorseFortunes < Formula
   desc "a fortune file of horse_ebooks"
   homepage "http://horsefortun.es"
   version "1.0.0"
-  url "http://files.arko.net/1u3d360l1Y2O/horse_ebooks"
+  url "https://raw.githubusercontent.com/indirect/homebrew-tap/master/share/horse_ebooks"
   sha256 "6d1d0f7876270b7d10c7676506823d21f48dff0da2569a3be70575e8299b5649"
 
   depends_on "fortune" => :build
