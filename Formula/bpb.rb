6bcf1571f00fabb086d5b9207ec08f640dc440e0
Andre Arko <andre@arko.net>
2022-01-11 17:37:48 -0800

    update bpb formula for 1.3.0

--- Formula/bpb.rb
+++ Formula/bpb.rb
@@ -1,14 +1,14 @@
 class Bpb < Formula
   desc "boats' personal barricade"
   homepage "https://github.com/withoutboats/bpb"
-  url "https://github.com/indirect/bpb/archive/de2b7be4eef4342ff54bd9bc84de0c5363264480.zip"
-  version "1.2.0"
-  sha256 "d24cf6c09e7497e5b38d85b6d52d51be9209163e44ac253ada6a84d801db5326"
+  url "https://github.com/indirect/bpb/archive/refs/tags/v1.3.0.zip"
+  version "1.3.0"
+  sha256 "709445fe34c0ac21448728db6814c90617daf8afef105ba0e72a8dc6b9e3611d"
 
   bottle do
-    root_url "https://github.com/indirect/homebrew-tap/releases/download/bpb-v1.2.0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f947366c4bb526a4c1d80d63dfca4b50ada8326279e738c47c375f1cc25d3bd8"
-    sha256 cellar: :any_skip_relocation, big_sur:       "7232cdecdf15817783ade2e1162e64d1937a3401fc6d799de733e348c451b509"
+    root_url "https://github.com/indirect/homebrew-tap/releases/download/bpb-v1.3.0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "32aab73b4787032370652c27329c828d33d0239492bc54bedc0dfa7bf366aaee"
+    sha256 cellar: :any_skip_relocation, monterey: "946359bcb8e97f63fdab8a52d27ad912e8e8c03748211da413c26b29a5ae7b37"
   end
 
   depends_on "rust" => :build
