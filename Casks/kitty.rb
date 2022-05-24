43d91500bdb792208769d187ff642a58eb2f1bba
Andre Arko <andre@arko.net>
2022-05-24 16:20:16 -0700

    fork Kitty to change the icon

---
+++ Casks/kitty.rb
@@ -0,0 +1,48 @@
+cask "kitty" do
+  version "0.25.0"
+  sha256 "0ee17b9f61a1cb6006b393083d3fde5983340b2155f681253f4365193402b01e"
+
+  url "https://github.com/kovidgoyal/kitty/releases/download/v#{version}/kitty-#{version}.dmg"
+  name "kitty"
+  desc "GPU-based terminal emulator"
+  homepage "https://github.com/kovidgoyal/kitty"
+
+  depends_on macos: ">= :sierra"
+
+  app "kitty.app"
+  # shim script (https://github.com/Homebrew/homebrew-cask/issues/18809)
+  shimscript = "#{staged_path}/kitty.wrapper.sh"
+  binary shimscript, target: "kitty"
+  binary "#{appdir}/kitty.app/Contents/Resources/terminfo/78/xterm-kitty",
+    target: "#{ENV.fetch("TERMINFO", "#{ENV["HOME"]}/.terminfo")}/78/xterm-kitty"
+
+  preflight do
+    File.write shimscript, <<~EOS
+      #!/bin/sh
+      exec '#{appdir}/kitty.app/Contents/MacOS/kitty' "$@"
+    EOS
+  end
+
+  postflight do
+    system_command "curl",
+      args: [
+        "https://github.com/k0nserv/kitty-icon/raw/main/kitty.icns",
+        "--output", "#{appdir}/kitty.app/Contents/Resources/kitty.icns"
+      ],
+      sudo: false
+    system_command "touch",
+      args: ["#{appdir}/kitty.app"],
+      sudo: false
+    system_command "xattr",
+      args: ["-cr", "#{appdir}/kitty.app"],
+      sudo: false
+  end
+
+  zap trash: [
+    "~/.config/kitty",
+    "~/Library/Caches/kitty",
+    "~/Library/Preferences/kitty",
+    "~/Library/Preferences/net.kovidgoyal.kitty.plist",
+    "~/Library/Saved Application State/net.kovidgoyal.kitty.savedState",
+  ]
+end
