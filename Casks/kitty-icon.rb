e76edf07a1b91199b0b376a76b5c13f54ebfd43d
Andre Arko <andre@arko.net>
2022-05-27 01:38:24 -0700

    uhh okay make the icon depend on the real cask

---
+++ Casks/kitty-icon.rb
@@ -0,0 +1,29 @@
+cask "kitty-icon" do
+  version "1.0"
+  sha256 "6f156e5a65d792d9a35f521f60a7dd8bedee55de479c417fa9cc2079515ace58"
+  url "https://github.com/k0nserv/kitty-icon/raw/main/kitty.icns"
+  name "kitty"
+  desc "A different indirectcon for Kitty"
+  homepage "https://github.com/indirect/homebrew-tap/"
+
+  depends_on cask: "kitty"
+  
+  binary "kitty.icns", target: "#{appdir}/kitty.app/Contents/Resources/kitty.icns"
+
+  preflight do
+    system_command "mv", args: [
+      "#{appdir}/kitty.app/Contents/Resources/kitty.icns",
+      "#{appdir}/kitty.app/Contents/Resources/kitty.icns.bak"
+    ], sudo: false
+  end
+
+  postflight do
+    puts "POSTFLIGHT"
+    system_command "touch",
+      args: ["#{appdir}/kitty.app"],
+      sudo: false
+    system_command "xattr",
+      args: ["-cr", "#{appdir}/kitty.app"],
+      sudo: false
+  end
+end
