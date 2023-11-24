5e2af45b815c46589967100d5717e8f824c866c5
Andre Arko <andre@arko.net>
2023-11-24 15:30:49 -0800

    ghostty cask

---
+++ Casks/ghostty.rb
@@ -0,0 +1,19 @@
+cask "ghostty" do
+  version :latest
+  sha256 :no_check
+  depends_on arch: :arm64, macos: ">= :monterey"
+
+  name "ghostty"
+  desc "GPU-accelerated terminal emulator pushing modern features"
+  homepage "https://github.com/mitchellh/ghostty"
+  url do
+    assets = GitHub.get_release("mitchellh", "ghostty", "tip").fetch("assets")
+    latest = assets.find{|a| a["name"] == "ghostty-macos-universal.zip" }.fetch("url")
+    [latest, header: [
+      "Accept: application/octet-stream",
+      "Authorization: bearer #{GitHub::API.credentials}"
+    ]]
+  end
+
+  app "Ghostty.app"
+end
