796e5f49c05ec5fc49922e1aff89e0522ae1202c
Justin Su <injustsu@gmail.com>
2024-08-08 03:56:49 -0400

    [ghostty] link fish completions

--- Casks/ghostty.rb
+++ Casks/ghostty.rb
@@ -18,6 +18,8 @@ cask "ghostty" do
 
   app "Ghostty.app"
   binary "#{appdir}/Ghostty.app/Contents/MacOS/ghostty"
+  binary "#{appdir}/Ghostty.app/Contents/Resources/fish/vendor_completions.d/ghostty.fish",
+         target: "#{HOMEBREW_PREFIX}/share/fish/vendor_completions.d/ghostty.fish"
   manpage "#{appdir}/Ghostty.app/Contents/Resources/man/man1/ghostty.1"
   manpage "#{appdir}/Ghostty.app/Contents/Resources/man/man5/ghostty.5"
 
